90b67aaeb162884ea89439b960f7db2bdf5faf90
shahim@essaid.com <shahim@essaid.com@7f0ee05a-44d7-5d01-afc4-268cbad96fb5>
2013-07-12 19:30:35 +0000

    Creating files for the three modules for the final release.
    git-svn-id: http://connect-isf.googlecode.com/svn@246 7f0ee05a-44d7-5d01-afc4-268cbad96fb5

---
+++ trunk/src/ontology/module/clinical-module.vue
@@ -0,0 +1,81 @@
+<!-- Tufts VUE 3.2 concept-map (clinical-module.vue) 2013-07-12 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Fri Jul 12 12:24:37 PDT 2013 by essaids on platform Windows 7 6.1 in JVM 1.6.0_29-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 14 2013 at 1717 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.6.0_39-b04(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="clinical-module.vue" created="1373657028877" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1373657077202"
+        spec="C:\s\svns\isf-new-layout\trunk\src\ontology\module\clinical-module.vue"
+        type="1" xsi:type="URLResource">
+        <title>clinical-module.vue</title>
+        <property key="File" value="C:\s\svns\isf-new-layout\trunk\src\ontology\module\clinical-module.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/d4574a7d0a85291f006258e18e9bbdbb</URIString>
+    <layer ID="1" label="Layer 1" created="1373657028888" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/d4574a7e0a85291f006258e17de0879a</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1373657028869"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/d4574a7e0a85291f006258e1b9e6e754</URIString>
+            <masterSlide ID="2" created="1373657028925" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/d4574a7f0a85291f006258e1ad9fde52</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1373657030054" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/d4574a7f0a85291f006258e1f37656b6</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1373657030055" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/d4574a800a85291f006258e1c376b26b</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1373657030057"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/d4574a800a85291f006258e113a0f7d6</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2013-07-12</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>C:\s\svns\isf-new-layout\trunk\src\ontology\module</saveLocation>
+    <saveFile>C:\s\svns\isf-new-layout\trunk\src\ontology\module\clinical-module.vue</saveFile>
+</LW-MAP>
