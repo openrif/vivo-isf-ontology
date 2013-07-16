f3025a38018791879f231e6eed4988e1dfb01d0d
shahim@essaid.com <shahim@essaid.com@7f0ee05a-44d7-5d01-afc4-268cbad96fb5>
2013-07-16 12:13:33 +0000

    Created a "release-tools" Eclipse project for ISF scripts and tools. It contains a draft script to build release modules.
    git-svn-id: http://connect-isf.googlecode.com/svn@250 7f0ee05a-44d7-5d01-afc4-268cbad96fb5

---
+++ trunk/src/tools/release-tools/src/isf/ISFUtil.java
@@ -0,0 +1,248 @@
+package isf;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAnnotationSubject;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLEntityVisitor;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.util.AutoIRIMapper;
+
+public class ISFUtil {
+
+	private static final OWLDataFactory df = OWLManager.getOWLDataFactory();
+
+	/**
+	 * The system property name for the root directory (the parent of trunk) of
+	 * the local SVN checkout. This is used by scripts to know where to load
+	 * from and create files.
+	 */
+	public static final String ISF_SVN_ROOT_DIR_PROPERTY = "isf.svn.root.dir";
+	public static final String ISF_ONTOLOGY_IRI_PREFIX = "http://purl.obolibrary.org/obo/arg/";
+
+	public static final IRI ISF_IRI = IRI.create(ISF_ONTOLOGY_IRI_PREFIX
+			+ "isf.owl");
+	public static final IRI ISF_FULL_IRI = IRI.create(ISF_ONTOLOGY_IRI_PREFIX
+			+ "isf-full.owl");
+
+	public static File getSvnRootDir() {
+		if (ISF_SVN_ROOT_DIR != null) {
+			return ISF_SVN_ROOT_DIR;
+		} else {
+			throw new IllegalStateException(
+					"isf.svn.root.dir system property not set or not valid.");
+		}
+	}
+
+	public static OWLOntology setupAndLoadIsfOntology(OWLOntologyManager man)
+			throws OWLOntologyCreationException {
+		setupManager(man);
+		man.loadOntology(ISF_IRI);
+		return man.getOntology(ISF_IRI);
+	}
+
+	public static OWLOntology setupAndLoadIsfFullOntology(OWLOntologyManager man)
+			throws OWLOntologyCreationException {
+		setupManager(man);
+		man.loadOntology(ISF_FULL_IRI);
+		return man.getOntology(ISF_FULL_IRI);
+	}
+
+	public static Set<OWLAnnotationAssertionAxiom> getAnnotationAxioms(
+			OWLOntology ontology, OWLAnnotationProperty property,
+			boolean includeImports) {
+		Set<OWLAnnotationAssertionAxiom> axioms = new HashSet<OWLAnnotationAssertionAxiom>();
+		Set<OWLOntology> ontologies;
+		if (includeImports) {
+			ontologies = ontology.getImportsClosure();
+		} else {
+			ontologies = Collections.singleton(ontology);
+		}
+		for (OWLOntology o : ontologies) {
+			for (OWLAnnotationAssertionAxiom aaa : o
+					.getAxioms(AxiomType.ANNOTATION_ASSERTION)) {
+				if (aaa.getProperty().getIRI().equals(property.getIRI())) {
+					axioms.add(aaa);
+				}
+			}
+		}
+		return axioms;
+	}
+
+	public static Set<OWLEntity> getSubsClosure(OWLEntity entity,
+			final OWLOntology ontology, final boolean includeImports) {
+		final Set<OWLEntity> entities = new HashSet<OWLEntity>();
+		entities.add(entity);
+		final Set<OWLOntology> ontologies;
+		if (includeImports) {
+			ontologies = ontology.getImportsClosure();
+		} else {
+			ontologies = Collections.singleton(ontology);
+		}
+
+		entity.accept(new OWLEntityVisitor() {
+			
+			@Override
+			public void visit(OWLAnnotationProperty property) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void visit(OWLDatatype datatype) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void visit(OWLNamedIndividual individual) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void visit(OWLDataProperty property) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void visit(OWLObjectProperty property) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void visit(OWLClass cls) {
+				for (OWLClassExpression ce : cls.getSubClasses(ontologies)) {
+					if (ce instanceof OWLClass) {
+						entities.add((OWLClass) ce);
+						entities.addAll(getSubsClosure((OWLClass) ce, ontology,
+								includeImports));
+					}
+				}
+				
+			}
+		});
+		
+		return entities;
+	}
+
+	public static Set<OWLAnnotationAssertionAxiom> getAnnotationAxioms(
+			OWLOntology ontology, boolean includeImports,
+			OWLAnnotationSubject subject) {
+		Set<OWLAnnotationAssertionAxiom> axioms = new HashSet<OWLAnnotationAssertionAxiom>();
+		Set<OWLOntology> ontologies;
+		if (includeImports) {
+			ontologies = ontology.getImportsClosure();
+		} else {
+			ontologies = Collections.singleton(ontology);
+		}
+		for (OWLOntology o : ontologies) {
+			axioms.addAll(o.getAnnotationAssertionAxioms(subject));
+		}
+		return axioms;
+
+	}
+
+	public static Set<OWLAxiom> getDefiningAxioms(final OWLEntity entity,
+			OWLOntology ontology, boolean includeImports) {
+		final Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+		Set<OWLOntology> ontologies;
+		if (includeImports) {
+			ontologies = ontology.getImportsClosure();
+		} else {
+			ontologies = Collections.singleton(ontology);
+		}
+
+		for (final OWLOntology o : ontologies) {
+			entity.accept(new OWLEntityVisitor() {
+
+				@Override
+				public void visit(OWLAnnotationProperty property) {
+					axioms.addAll(o.getAxioms(property));
+				}
+
+				@Override
+				public void visit(OWLDatatype datatype) {
+					axioms.addAll(o.getAxioms(datatype));
+
+				}
+
+				@Override
+				public void visit(OWLNamedIndividual individual) {
+					axioms.addAll(o.getAxioms(individual));
+
+				}
+
+				@Override
+				public void visit(OWLDataProperty property) {
+					axioms.addAll(o.getAxioms(property));
+
+				}
+
+				@Override
+				public void visit(OWLObjectProperty property) {
+					axioms.addAll(o.getAxioms(property));
+
+				}
+
+				@Override
+				public void visit(OWLClass cls) {
+					axioms.addAll(o.getAxioms(cls));
+
+				}
+			});
+		}
+
+		return axioms;
+	}
+
+	private static void setupManager(OWLOntologyManager man) {
+		AutoIRIMapper mapper = new AutoIRIMapper(new File(getSvnRootDir(),
+				"trunk/src/ontology"), true);
+		man.clearIRIMappers();
+		man.addIRIMapper(mapper);
+	}
+
+	private static File ISF_SVN_ROOT_DIR = null;
+
+	static {
+		String svnRoot = System.getProperty(ISF_SVN_ROOT_DIR_PROPERTY);
+		if (svnRoot != null) {
+			File svnRootDir = new File(svnRoot).getAbsoluteFile();
+			if (checkValidSvnLocation(svnRootDir)) {
+				ISF_SVN_ROOT_DIR = svnRootDir;
+			}
+		}
+	}
+
+	private static boolean checkValidSvnLocation(File svnRootDir) {
+		if (svnRootDir.isDirectory()) {
+			File tools = new File(svnRootDir, "trunk/src/tools");
+			if (tools.exists()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ trunk/src/tools/release-tools/src/isf/module/BuildModule.java
@@ -0,0 +1,227 @@
+package isf.module;
+
+import isf.ISFUtil;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.RDFXMLOntologyFormat;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+public class BuildModule {
+
+	private OWLOntologyManager isfFullMan = OWLManager
+			.createOWLOntologyManager();
+	private OWLDataFactory df = isfFullMan.getOWLDataFactory();
+	private OWLOntology isfFullOntology;
+
+	private OWLOntology moduleOntology;
+	private OWLOntology moduleOntologyInclude;
+	private OWLOntology moduleOntologyExclude;
+	private OWLOntology moduleOntologyGenerated;
+	private String[] args;
+	private Set<OWLOntology> changedOntologies = new HashSet<OWLOntology>();
+
+	private void run(String[] args) throws OWLOntologyCreationException,
+			OWLOntologyStorageException {
+		this.args = args;
+		inti();
+
+		addIncludes();
+		addIncludeSubs();
+
+		removeExcludes();
+		removeExcludeSubs();
+
+		mergeModuleInclude();
+
+		addClosureToBfo();
+
+		addAnnotations();
+		typeAllEntities();
+
+		save();
+	}
+
+	public void addIncludes() {
+		Set<OWLEntity> entities = ModuleUtil.getIncludeEntities(moduleOntology,
+				true);
+
+		for (OWLEntity e : entities) {
+			addAxiom(df.getOWLDeclarationAxiom(e));
+			addAxioms(ISFUtil.getDefiningAxioms(e, isfFullOntology, true));
+		}
+
+	}
+
+	public void addIncludeSubs() {
+		Set<OWLEntity> entities = ModuleUtil.getIncludeSubsEntities(
+				moduleOntology, true);
+		Set<OWLEntity> closureEntities = new HashSet<OWLEntity>();
+
+		for (OWLEntity e : entities) {
+			closureEntities.addAll(ISFUtil.getSubsClosure(e, isfFullOntology,
+					true));
+		}
+		for (OWLEntity e : closureEntities) {
+			addAxiom(df.getOWLDeclarationAxiom(e));
+			addAxioms(ISFUtil.getDefiningAxioms(e, isfFullOntology, true));
+		}
+	}
+
+	public void removeExcludeSubs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void removeExcludes() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mergeModuleInclude() {
+		addAxioms(moduleOntologyInclude.getAxioms());
+	}
+
+	public void addClosureToBfo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addAnnotations() {
+		Set<OWLEntity> entitiesToAnnotate = new HashSet<OWLEntity>();
+		entitiesToAnnotate.addAll(moduleOntologyGenerated.getSignature());
+
+		Set<OWLEntity> annotatedEntities = new HashSet<OWLEntity>();
+
+		while (entitiesToAnnotate.size() > 0) {
+			Set<OWLEntity> newEntities = new HashSet<OWLEntity>();
+			Iterator<OWLEntity> i = entitiesToAnnotate.iterator();
+			while (i.hasNext()) {
+				OWLEntity entity = i.next();
+				i.remove();
+				annotatedEntities.add(entity);
+				Set<OWLAnnotationAssertionAxiom> axioms = ISFUtil
+						.getAnnotationAxioms(isfFullOntology, true,
+								entity.getIRI());
+				addAxioms(axioms);
+				for (OWLAnnotationAssertionAxiom a : axioms) {
+					Set<OWLEntity> signature = a.getSignature();
+					signature.removeAll(annotatedEntities);
+					newEntities.addAll(signature);
+				}
+			}
+
+			entitiesToAnnotate.addAll(newEntities);
+		}
+
+	}
+
+	public void typeAllEntities() {
+		for (OWLEntity e : moduleOntologyGenerated.getSignature()) {
+			addAxiom(df.getOWLDeclarationAxiom(e));
+		}
+
+	}
+
+	private void addAxioms(Set<? extends OWLAxiom> axioms) {
+		for (OWLAxiom axiom : axioms) {
+			addAxiom(axiom);
+		}
+	}
+
+	private void addAxiom(OWLAxiom axiom) {
+		if (!moduleOntologyExclude.containsAxiom(axiom)
+				&& !moduleOntologyInclude.containsAxiom(axiom)) {
+			isfFullMan.addAxiom(moduleOntologyGenerated, axiom);
+		}
+	}
+
+	private void save() throws OWLOntologyStorageException {
+		RDFXMLOntologyFormat format = new RDFXMLOntologyFormat();
+		format.setAddMissingTypes(true);
+		for (OWLOntology ontology : changedOntologies) {
+			System.out.println("Saving ontology: " + ontology.getOntologyID());
+			isfFullMan.saveOntology(ontology, format);
+		}
+
+	}
+
+	private void inti() throws OWLOntologyCreationException {
+		isfFullOntology = ISFUtil.setupAndLoadIsfFullOntology(isfFullMan);
+		// System.out.println(isfFullMan.getOntologies().size());
+		// for(OWLOntology o : isfFullMan.getOntologies()){
+		// System.out.println(o.getOntologyID() +
+		// " -> "+isfFullMan.getOntologyDocumentIRI(o));
+		// }
+		String moduleName = args[0];
+		// load module ontology
+		IRI moduleIri = IRI.create(ISFUtil.ISF_ONTOLOGY_IRI_PREFIX + moduleName
+				+ "-module.owl");
+		moduleOntology = getLoadCreateOntology(moduleIri);
+
+		IRI moduleIncludeIri = IRI.create(ISFUtil.ISF_ONTOLOGY_IRI_PREFIX
+				+ moduleName + "-module-include.owl");
+		moduleOntologyInclude = getLoadCreateOntology(moduleIncludeIri);
+
+		IRI moduleExcludeIri = IRI.create(ISFUtil.ISF_ONTOLOGY_IRI_PREFIX
+				+ moduleName + "-module-exclude.owl");
+		moduleOntologyExclude = getLoadCreateOntology(moduleExcludeIri);
+
+		// always create a new one
+		IRI moduleGeneratedIri = IRI.create(ISFUtil.ISF_ONTOLOGY_IRI_PREFIX
+				+ moduleName + "-module-generated.owl");
+		moduleOntologyGenerated = createOntology(moduleGeneratedIri);
+
+	}
+
+	private OWLOntology getLoadCreateOntology(IRI iri)
+			throws OWLOntologyCreationException {
+		OWLOntology ontology = isfFullMan.getOntology(iri);
+		if (ontology == null) {
+			try {
+				ontology = isfFullMan.loadOntology(iri);
+			} catch (OWLOntologyCreationException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		if (ontology == null) {
+			ontology = createOntology(iri);
+		}
+
+		return ontology;
+	}
+
+	private OWLOntology createOntology(IRI iri)
+			throws OWLOntologyCreationException {
+		OWLOntology ontology = isfFullMan.createOntology(iri);
+		int i = iri.toString().lastIndexOf('/');
+
+		String fileName = iri.toString().substring(i + 1);
+		File documentFile = new File(ISFUtil.getSvnRootDir(),
+				"trunk/src/ontology/module/" + fileName);
+		isfFullMan.setOntologyDocumentIRI(ontology,
+				IRI.create(documentFile.toURI()));
+		changedOntologies.add(ontology);
+		return ontology;
+	}
+
+	public static void main(String[] args) throws OWLOntologyCreationException,
+			OWLOntologyStorageException {
+
+		new BuildModule().run(args);
+
+	}
+
+}
